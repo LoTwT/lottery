35ffea991409f4020bb5e5f312b5a33f0248daf3
LoTwT <709937065@qq.com>
2021-08-22 20:03:40 +0800

    router init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <template>
-  <div class="app-container">a</div>
+  <router-view></router-view>
 </template>
 
 <script lang="ts" setup>
 
 </script>
 
-<style>
-.app-container {
-  width: 800px;
-  height: 600px;
-  background-color: aqua;
-}
+<style scoped>
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router"
 
+import Login from "@/views/Login.vue"
+import Admin from "@/views/Admin.vue"
+import User from "@/views/User.vue"
+
 export default createRouter({
     history: createWebHistory("/"),
-    routes: [],
+    routes: [
+        { path: "/", component: Login },
+        { path: "/admin", component: Admin, name: "admin" },
+        { path: "/user", component: User, name: "user" },
+    ],
 })
