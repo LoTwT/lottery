88eb83e64f98fbe1d2a79afc27ebf5636dcfe081
LoTwT <709937065@qq.com>
2021-08-22 19:43:25 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-container">a</div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+.app-container {
+  width: 800px;
+  height: 600px;
+  background-color: aqua;
+}
+</style>
